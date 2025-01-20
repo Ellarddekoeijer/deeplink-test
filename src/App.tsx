74363812d45501bb5dffee8a4a3ab343148f2ba4
Ellard <Ellard.dekoeijer@perplex.nl>
2025-01-20 11:23:23 +0100

    Added link to app

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+          <a href="https://deeplink-test-pearl.vercel.app/app">App link</a>
       </p>
     </>
   )
